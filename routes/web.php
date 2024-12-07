0ce7f20cdfa9bfb7b81a1c958a115a3400a4a257
TranTienAnh2012 <tienanhtran755@gmail.com>
2024-12-07 23:59:23 +0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TTalogincontroller;
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,4 +21,27 @@ Route::get('/', function () {
 Route::get('/login',[TTalogincontroller::class,'ttaindex'])->name('ttalogin.ttaindex');
 Route::post('/login',[TTalogincontroller::class,'ttaSubmit'])->name('ttalogin.ttaSubmit');
 
-
+//lad5d
+//lấy đường dẫn
+Route::get('/user',function(Request $request){
+    return $request->path();
+});
+//lấy full đường dẫn $res=$request->fullUrlWithoutQuery(['email']);
+Route::get('/users',function(Request $request){
+    return $request->fullUrl();
+    $res=$request->fullUrlWithoutQuery(['email']);
+});
+// Request Host
+Route::get('/host-test',function(Request $request){
+    $host=$request->host();
+    $httpHost=$request->httpHost();
+    $schemeAndHttpHost=$request->schemeAndHttpHost();
+    return "<h2> Host: ".$host
+    . "<h2> httpHost: ".$httpHost
+    . "<h2> schemeAndHttpHost: ".$schemeAndHttpHost;
+});
+//nhận các phương thức qua đối tương request
+Route::get('/method-test', function(Request $request){
+    $res=$request->method();
+    return $res;
+});
